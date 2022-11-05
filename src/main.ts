fbd5f91675511e4eb30e1cbac066c8ab88957651
Jared Meadows <meadowsjared@gmail.com>
2022-11-05 04:26:38 -0700

    feat: add firebase to app
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import App from './App.vue';
+import Index from './Index.vue';
 import router from './router';
 import './main.css';
 
 (import.meta as ImportMeta & { env: any }).env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
 
-const app = createApp(App);
+const app = createApp(Index);
 
 app.use(createPinia()); //use pinia
 app.use(router); //use router
