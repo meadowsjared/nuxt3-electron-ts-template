b781f3488550462f88db848272992fab1fb6f730
Jared Meadows <meadowsjared@gmail.com>
2022-11-04 18:18:53 -0700

    chore: updated all packages

--- src/electron/main/main.ts
+++ src/electron/main/main.ts
@@ -1,52 +1,50 @@
 import { join } from 'path';
-import {
-    app,
-    BrowserWindow
-} from 'electron';
+import { app, BrowserWindow } from 'electron';
 
-const isDev = process.env.npm_lifecycle_event === "app:dev" ? true : false;
+const isDev = process.env.npm_lifecycle_event === 'app:dev' ? true : false;
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
 
 function createWindow() {
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: join(__dirname, '../preload/preload.js'),
-        },
-    });
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: join(__dirname, '../preload/preload.js'),
+    },
+  });
 
-    // and load the index.html of the app.
-    if (isDev) {
-        mainWindow.loadURL('http://localhost:3000');// Open the DevTools.
-        mainWindow.webContents.openDevTools();
-    } else {
-        mainWindow.loadFile(join(__dirname, '../../index.html'));
-    }
-    // mainWindow.loadURL( //this doesn't work on macOS in build and preview mode
-    //     isDev ?
-    //     'http://localhost:3000' :
-    //     join(__dirname, '../../index.html')
-    // );
+  // and load the index.html of the app.
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000'); // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(join(__dirname, '../../index.html'));
+  }
+  // mainWindow.loadURL( //this doesn't work on macOS in build and preview mode
+  //     isDev ?
+  //     'http://localhost:3000' :
+  //     join(__dirname, '../../index.html')
+  // );
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    createWindow()
-    app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+  createWindow();
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+(import.meta as ImportMeta & { env: any }).env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
 
-app.use(createPinia()) //use pinia
-app.use(router) //use router
-app.mount('#app')
+const app = createApp(App);
+
+app.use(createPinia()); //use pinia
+app.use(router); //use router
+app.mount('#app');
