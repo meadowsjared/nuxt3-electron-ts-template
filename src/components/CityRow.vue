e9035295cbac33d6700f0e2534bcecd0897ed123
Jared Meadows <meadowsjared@gmail.com>
2022-11-28 02:54:44 -0800

    feat: CityRow now saves using Pinia store (AND firebase) 🎉
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/components/CityRow.vue
+++ src/components/CityRow.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="flex justify-center gap-4">
     <div>{{ index }}</div>
-    <input v-model="city.name" />
+    <input v-model="city.name" @update:model-value="updatedCity" />
+    <button @click="cityStore.deleteCity(props.city)" title="delete city">❌</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { City } from '../domain/City';
+import { useCityStore } from '../store/city';
 
-defineProps<{
+const props = defineProps<{
   city: City;
+  index: number;
 }>();
+
+const cityStore = useCityStore();
+
+function updatedCity(newCityName: string) {
+  const newCity: City = {
+    ...props.city,
+    name: newCityName,
+  };
+
+  cityStore.saveCity(newCity);
+}
 </script>
