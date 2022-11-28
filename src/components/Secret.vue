a23a41c46f269cfb99171a17886c739c7af913f0
Jared Meadows <meadowsjared@gmail.com>
2022-11-28 02:55:45 -0800

    update: Secrete.vue to use Pinia store 🍻
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/components/Secret.vue
+++ src/components/Secret.vue
@@ -1,47 +1,27 @@
 <template>
   <div>
-    <h1>This is secret info {{ props.q }}</h1>
-    <city-row v-for="city in cities" :key="city.id" :city="city" />
+    <h1>This is secret info</h1>
+    <city-row v-for="(city, index) in cityStore.cities" :key="city.id" :city="city" :index="index" />
     <button @click="addCity('stuff')">add stuff</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onValue, set } from 'firebase/database';
-import { ref } from 'vue';
 import { useRouter } from 'vue-router';
-import { City } from '../domain/City';
-import { auth, citiesRef } from '../plugins/firebase';
-
-const props = defineProps<{
-  q?: string;
-}>();
+import { auth } from '../plugins/firebase';
+import { useCityStore } from '../store/city';
 
 const router = useRouter();
-
-const cities = ref([] as City[]);
+const cityStore = useCityStore();
 
 if (auth.currentUser === null) {
   router.push({ path: '/login', query: { q: 'test' } });
 } else {
-  onValue(citiesRef, snapshot => {
-    const data = snapshot.val() as City[];
-    cities.value = data.filter(value => value !== undefined);
-    console.log('cities.value', cities.value);
-  });
-
-  console.log('Secret page loaded', props.q);
+  cityStore.loadCities();
+  console.log('Secret page loaded');
 }
 
 function addCity(cityName: string) {
-  set(citiesRef, [...cities.value, cityName])
-    .then(() => {
-      // Data saved successfully!
-      console.log('Data saved successfully!');
-    })
-    .catch(error => {
-      // The write failed...
-      console.warn('error', error);
-    });
+  cityStore.addCity(cityName);
 }
 </script>
