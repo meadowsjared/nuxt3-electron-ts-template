282dcd916aa7de8ac7eab92f924b68563d56693c
Jared Meadows <meadowsjared@gmail.com>
2022-12-11 06:06:56 -0800

    feat: add InlineSvg's to + and - buttons & styling
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
+import InlineSvg from "vue-inline-svg";
+import addIcon from '../assets/images/streamlinehq-add-thick-interface-essential-50.svg';
+import subtractIcon from '../assets/images/streamlinehq-subtract-thick-interface-essential-50.svg';
 import { useCounterStore } from '../store/counter';
 
 const counterStore = useCounterStore();
 </script>
 
 <template>
-  <button type="button" @click="counterStore.add()" class="bg-lunarRock rounded px-3 my-4">count is: {{ counterStore.count }}</button>
+  <div class="flex items-center justify-center gap-2">
+    count is: {{ counterStore.count }}
+    <button type="button" @click="counterStore.add()" class="bg-arubaGreen rounded px-3 py-1 my-4 text-white active:bg-greenSeduction active:text-neutralLight-400">
+      <inline-svg :src="addIcon" class="w-6 h-6" />
+    </button>
+    <button type="button" @click="counterStore.subtract()" class="bg-arubaGreen rounded px-3 py-1 my-4 text-white active:bg-greenSeduction active:text-neutralLight-400">
+      <inline-svg :src="subtractIcon" class="w-6 h-6" />
+    </button>
+  </div>
+
   <router-view />
 </template>
